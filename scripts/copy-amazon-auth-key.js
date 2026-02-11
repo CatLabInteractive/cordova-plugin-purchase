f227e5db323a372c888b3b746ee7b739451462e1
Thijs Van der Schaeghe <thijs@catlab.be>
2026-02-11 01:07:56 +0100

    Add some debug code to the hook.

--- scripts/copy-amazon-auth-key.js
+++ scripts/copy-amazon-auth-key.js
@@ -17,8 +17,11 @@ module.exports = function (context) {
     var destDir = path.join(projectRoot, 'platforms', 'android', 'app', 'src', 'main', 'assets');
     var destFile = path.join(destDir, 'AppstoreAuthenticationKey.pem');
 
+    console.log('Looking for AppstoreAuthenticationKey.pem in ' + projectRoot + ' and copying it to ' + destDir + ' if it exists.');
+
     if (!fs.existsSync(srcFile)) {
         // File not present in project root — nothing to do.
+        console.log('AppstoreAuthenticationKey.pem not found in project root. Skipping copy.');
         return;
     }
 
